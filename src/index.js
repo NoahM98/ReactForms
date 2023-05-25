d95815ddcdd3a2d5b3bd8bc73774ea967580c76b
Noah Mauer <noahmauer1998@gmail.com>
2023-05-25 19:51:13 -0400

    Added functionality to the form

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Form = () => {
+  const [username, setUsername] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    console.log(username);
+  }
   return (
     <div id='container'>
       <div id='navbar'>
         Form.js
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* your form fields here */}
+        <label htmlFor="username"></label>
+        <input type="text" name="username" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
